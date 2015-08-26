31cf586faf16a7708146d5d7e8e01b0e4550777d
Mike Taylor <kovarid@gmail.com>
2015-08-26 12:05:48 -0400

    Create versioninfo.lua

---
+++ versioninfo.lua
@@ -0,0 +1,86 @@
+-- ATS MC Plugins Version info
+version_info = 
+{ 
+	["de6b99ef3f79191fd27d59b0"] = 
+	{ 
+		name = "ChatGrabberMain", version = 2.20, page = "http://qon.pennmush.org/ats/beta/",
+		filename = "ATS_Chatgrabber.xml", infopage = "http://qon.pennmush.org/chatgrabber/",
+        whatsnew = "(v2.20) Hyperlinks display properly in the main window.\n* AutoURL Shortener option -- When enabled, any URL you type in the command window (and hit enter) will be tinyurl.com'd automagically.",
+	}, 
+	["981f20859e43e90c624fa1ff"] = 
+	{
+		name = "ChatGrabberSlave1", version = 2.20, page = "http://qon.pennmush.org/ats/beta/",
+		filename = "ATS_ChatgrabberSlave1.xml",  infopage = "http://qon.pennmush.org/chatgrabber/",
+	},
+	["d183a293b696885487d3ad83"] =
+	{
+		name = "ChatGrabberSlave2", version = 2.20, page = "http://qon.pennmush.org/ats/beta/", 
+		filename = "ATS_ChatgrabberSlave2.xml", infopage = "http://qon.pennmush.org/chatgrabber/",
+		
+	},
+	["8e3fd4f9c269365839520477"] = 
+	{
+		name = "ATS_Navcomp", version = 1.41, page = "http://qon.pennmush.org/ats/beta/",
+		filename = "ATS_Navcomp.xml",  infopage = "http://qon.pennmush.org/ats-navcomp/",
+        whatsnew = "(v1.4) AutoTuning calculator (type 'sheet', then @listspecs a class)\nNew JSON Datafile (faster loading)\n ncinfo command. (try 'ncinfo fed') More to come. ",
+	},
+	["1ec11b537d60054d832b896a"] = 
+	{
+		name = "QUpdater", version = 0.6, page = "http://qon.pennmush.org/ats/beta/", 
+		filename = "QUpdater.xml", infopage = "http://qon.pennmush.org/qupdater/",
+	},
+	["57a49cca2ef1da0b52498a94"] =
+	{
+		name = "ShieldStatus", version = 1.02, page = "http://qon.pennmush.org/ats/beta/",
+		filename = "ATS_ShieldStatus.xml", 
+	},
+	["14f4885e803e076273a4767a"] = 
+	{
+		name = "Paste_As_MUSH", version = 1.22, page = "http://qon.pennmush.org/ats/beta/",
+		filename = "Paste_As_MUSH.xml", infopage = nil,
+	},
+	["NavcompData"] = 
+	{
+		name = "NavComp Database", version = 2.01, page = "http://qon.pennmush.org/ats/beta/",
+		filename = "atsdata.json",  infopage = "http://qon.pennmush.org/ats-navcomp/",
+	},
+    ["f92c816b178f0707847fcd1d"] =
+    {
+        name = "WeaponBar 2.0 ALPHA", version = 2.06, page = "http://qon.pennmush.org/ats/beta/",
+        filename = "ATS_WeaponBarTest.xml", infopage = nil,
+        whatsnew = "(2.0) Use the right-click menu to add/edit/delete buttons and bars.(v2.05) Added Preset-Save. New bars appear center screen.",
+        requires = { "DisruptorCannon.png", "TricobaltAft.png", "Tricobalt.png" },
+    },
+}
+
+
+
+ats_navcomp_info = {
+	plugin_version = "1.15",
+	db_version = "1.03",
+	release_date = "12/23/14 9:24am EDT",
+	release_url = "http://qon.pennmush.org/mediawiki/index.php/ATS_Nav_Comp_Plugin",
+	description = [==[ 
+	
+	* (Pre-1.15)Added support for unofficial borders (e.g. QI'Tomer, Beeblebrox, etc.)
+	* (Pre-1.15)Debugmode can now be toggled by typing 'atsdebug' (defaults to off)
+	* (Pre-1.15)Some fixes here and there, including speed optimizations.
+	* (Pre-1.15)Added spam filter test code (only works if you don't have chatgrabber installed)
+	* (Pre-1.15)near sorts nearest objects by distance, from nearest to furthest
+	* (1.15) You can now click on planet names from the 'plot/from' command, to see nearby objects.
+	* (1.16) Fixed a few more bugs. Adds condense scan option. (Accessibility)
+	* (1.16) Lots of command name changes, see 'nchelp' to see the updated list.
+	* (1.16) Fixed stats tracker. You should do 'spacestats clear' after updating or weird things may happen.
+	* (1.16) Now filters Tractor spam as well -- Note if you have chatgrabber installed, it handles this function
+	* If you have chatgrabber installed, but do not have subspace going to a miniwindow, spam filtering WILL NOT WORK.
+	]==],
+	db_description = [==[
+	* Fixes
+	* Adds support for the unofficial borders.
+	*** DO NOT USE THIS DB WITH NON BETA NAVCOMP. IT WILL CRASH.
+	*** INFACT, YOU MAY CRASH UNLESS YOU UPDATE TO BETA 1.14!
+	]==],
+	
+}
+
+
