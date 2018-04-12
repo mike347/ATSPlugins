72485ecff719ff48ff90c32705a1e42c0390f725
Mike Taylor <kovarid@gmail.com>
2018-04-12 16:05:00 -0400

    Updating from local changes
    Lots of changes, not going to detail them.. Setting this up so that it can be properly version tracked going forward.

--- versioninfo.lua
+++ versioninfo.lua
@@ -1,56 +1,65 @@
--- ATS MC Plugins Version info
+-- ATS Navcomp Version info
 version_info = 
 { 
 	["de6b99ef3f79191fd27d59b0"] = 
 	{ 
-		name = "ChatGrabberMain", version = 2.20, page = "http://qon.pennmush.org/ats/beta/",
-		filename = "ATS_Chatgrabber.xml", infopage = "http://qon.pennmush.org/chatgrabber/",
-        whatsnew = "(v2.20) Hyperlinks display properly in the main window.\n* AutoURL Shortener option -- When enabled, any URL you type in the command window (and hit enter) will be tinyurl.com'd automagically.",
+		name = "ChatGrabberMain", version = 2.23, page = "http://zen.trekmush.org/ats/beta/",
+		filename = "ATS_Chatgrabber.xml", infopage = "http://zen.trekmush.org/chatgrabber/",
+        whatsnew = "(2.23) Reduced max stored lines to curtail crashing on some systems.",
 	}, 
 	["981f20859e43e90c624fa1ff"] = 
 	{
-		name = "ChatGrabberSlave1", version = 2.20, page = "http://qon.pennmush.org/ats/beta/",
-		filename = "ATS_ChatgrabberSlave1.xml",  infopage = "http://qon.pennmush.org/chatgrabber/",
+		name = "ChatGrabberSlave1", version = 2.23, page = "http://zen.trekmush.org/ats/beta/",
+		filename = "ATS_ChatgrabberSlave1.xml",  infopage = "http://zen.trekmush.org/chatgrabber/",
 	},
 	["d183a293b696885487d3ad83"] =
 	{
-		name = "ChatGrabberSlave2", version = 2.20, page = "http://qon.pennmush.org/ats/beta/", 
-		filename = "ATS_ChatgrabberSlave2.xml", infopage = "http://qon.pennmush.org/chatgrabber/",
+		name = "ChatGrabberSlave2", version = 2.23, page = "http://zen.trekmush.org/ats/beta/", 
+		filename = "ATS_ChatgrabberSlave2.xml", infopage = "http://zen.trekmush.org/chatgrabber/",
 		
 	},
 	["8e3fd4f9c269365839520477"] = 
 	{
-		name = "ATS_Navcomp", version = 1.41, page = "http://qon.pennmush.org/ats/beta/",
-		filename = "ATS_Navcomp.xml",  infopage = "http://qon.pennmush.org/ats-navcomp/",
-        whatsnew = "(v1.4) AutoTuning calculator (type 'sheet', then @listspecs a class)\nNew JSON Datafile (faster loading)\n ncinfo command. (try 'ncinfo fed') More to come. ",
+		name = "ATS_Navcomp", version = 1.58, page = "http://zen.trekmush.org/ats/beta/",
+		filename = "ATS_Navcomp.xml",  infopage = "http://zen.trekmush.org/ats-navcomp/",
+        whatsnew = "Fixes TWG->Wormhole interactions. Added new TWG's",
 	},
 	["1ec11b537d60054d832b896a"] = 
 	{
-		name = "QUpdater", version = 0.6, page = "http://qon.pennmush.org/ats/beta/", 
-		filename = "QUpdater.xml", infopage = "http://qon.pennmush.org/qupdater/",
+		name = "QUpdater", version = 0.93, page = "http://zen.trekmush.org/ats/beta/", 
+		filename = "QUpdater.xml", infopage = "http://zen.trekmush.org/qupdater/",
 	},
 	["57a49cca2ef1da0b52498a94"] =
 	{
-		name = "ShieldStatus", version = 1.02, page = "http://qon.pennmush.org/ats/beta/",
+		name = "ShieldStatus", version = 1.02, page = "http://zen.trekmush.org/ats/beta/",
 		filename = "ATS_ShieldStatus.xml", 
 	},
 	["14f4885e803e076273a4767a"] = 
 	{
-		name = "Paste_As_MUSH", version = 1.22, page = "http://qon.pennmush.org/ats/beta/",
+		name = "Paste_As_MUSH", version = 1.22, page = "http://zen.trekmush.org/ats/beta/",
 		filename = "Paste_As_MUSH.xml", infopage = nil,
 	},
 	["NavcompData"] = 
 	{
-		name = "NavComp Database", version = 2.01, page = "http://qon.pennmush.org/ats/beta/",
-		filename = "atsdata.json",  infopage = "http://qon.pennmush.org/ats-navcomp/",
+		name = "NavComp Database", version = 2.15, page = "http://zen.trekmush.org/ats/beta/",
+		filename = "atsdata.json",  infopage = "http://zen.trekmush.org/ats-navcomp/",
 	},
     ["f92c816b178f0707847fcd1d"] =
     {
-        name = "WeaponBar 2.0 ALPHA", version = 2.06, page = "http://qon.pennmush.org/ats/beta/",
+        name = "WeaponBar 2.0 ALPHA", version = 2.06, page = "http://zen.trekmush.org/ats/beta/",
         filename = "ATS_WeaponBarTest.xml", infopage = nil,
         whatsnew = "(2.0) Use the right-click menu to add/edit/delete buttons and bars.(v2.05) Added Preset-Save. New bars appear center screen.",
         requires = { "DisruptorCannon.png", "TricobaltAft.png", "Tricobalt.png" },
     },
+--[[    
+    ["74f8c420df7d59ad5aa66246"] = 
+    {
+        name = "GMCP ALPHA DO NOT INSTALL", version = 1.0, page = "http://zen.trekmush.org/ats/beta/",
+        filename = "GMCP_handler.xml", infopage = nil,
+        whatsnew = "Testing. Don't install without being told to do so.",
+        requires = { "gmcphelper.lua" },
+    },
+--]]
 }
 
 
@@ -59,7 +68,7 @@ ats_navcomp_info = {
 	plugin_version = "1.15",
 	db_version = "1.03",
 	release_date = "12/23/14 9:24am EDT",
-	release_url = "http://qon.pennmush.org/mediawiki/index.php/ATS_Nav_Comp_Plugin",
+	release_url = "http://zen.trekmush.org/mediawiki/index.php/ATS_Nav_Comp_Plugin",
 	description = [==[ 
 	
 	* (Pre-1.15)Added support for unofficial borders (e.g. QI'Tomer, Beeblebrox, etc.)
